ee5952fc575e2090dc7d9fcb3b060fa5e0939cd5
Saad Qayyum <s.qayyum2003@gmail.com>
2023-09-15 20:30:17 +0100

    made calculator work with basic checks

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,16 +1,21 @@
 fetchTheme("salad").then(applyTheme);
-
 const display = document.querySelector(".display > .digit");
 display.textContent = 0;
 
 const operatorDisplay = document.querySelector(".display > .operator");
 operatorDisplay.textContent = "";
 
+let runningTotal = 0;
+let currentOperator = null;
+let clearScreen = false;
 
 const ac = document.querySelector(".ac");
-ac.addEventListener("click", (e) => {
+ac.addEventListener("click", () => {
     display.textContent = 0;
     operatorDisplay.textContent = "";
+    runningTotal = 0;
+    currentOperator = null;
+    clearScreen = false;
 });
 
 const numbers = document.querySelectorAll(".number");
@@ -18,15 +23,97 @@ numbers.forEach((number) => {
     number.addEventListener("click", updateDisplayScreen);
 });
 
+const opp = document.querySelectorAll(".opp");
+opp.forEach((op) => {
+    op.addEventListener("click", operatorUpdate);
+});
 
-function updateDisplayScreen(e){
-    const number = e.target;
-    let displayNumber = display.textContent;
-    displayNumber += number.textContent;
-    displayNumber = +displayNumber;
+const equal = document.querySelector(".eq");
+equal.addEventListener("click", performEquals);
 
-    if (displayNumber.toString().length <= 12) {
-        display.textContent = displayNumber;
+document.addEventListener("keydown", updateDisplayWindow);
+
+function updateDisplayScreen(e) {
+    const number = e.target.textContent;
+
+    if (clearScreen || display.textContent === '0') {
+        display.textContent = number;
+        clearScreen = false;
+    } else {
+        display.textContent += number;
+    }
+
+    if (display.textContent.length > 12) {
+        display.textContent = display.textContent.slice(0, 12);
     }
 }
 
+
+function operatorUpdate(e) {
+    if (currentOperator) {
+        performOperation();
+        display.textContent = runningTotal;
+    } else {
+        runningTotal = Number(display.textContent);
+    }
+
+    currentOperator = e.target.textContent;
+    operatorDisplay.textContent = currentOperator;
+    clearScreen = true;
+}
+
+function performOperation() {
+    switch (currentOperator) {
+        case "+":
+            runningTotal += Number(display.textContent);
+            break;
+        case "-":
+            runningTotal -= Number(display.textContent);
+            break;
+        case "*":
+            runningTotal *= Number(display.textContent);
+            break;
+        case "/":
+            runningTotal /= Number(display.textContent);
+            break;
+    }
+}
+
+function performEquals() {
+    if (currentOperator) {
+        performOperation();
+        display.textContent = runningTotal;
+        currentOperator = null;
+        operatorDisplay.textContent = "";
+    }
+    clearScreen = true;
+}
+
+function updateDisplayWindow(e) {
+    const keys = ["Digit0", "Digit1", "Digit2", "Digit3", "Digit4", "Digit5", "Digit6", "Digit7", "Digit8", "Digit9"];
+    const numpads = ["Numpad0", "Numpad1", "Numpad2", "Numpad3", "Numpad4", "Numpad5", "Numpad6", "Numpad7", "Numpad8", "Numpad9"];
+    const operators = {
+        "NumpadAdd": "+",
+        "NumpadSubtract": "-",
+        "NumpadMultiply": "*",
+        "NumpadDivide": "/"
+    };
+    const pressedKey = e.code;
+
+    if (keys.includes(pressedKey) || numpads.includes(pressedKey)) {
+        const number = pressedKey.slice(-1);
+        updateDisplayScreen({ target: { textContent: number } });
+    } else if (pressedKey === "Backspace") {
+        let displayNumber = display.textContent;
+        if (displayNumber.length === 1) {
+            displayNumber = "0";
+        } else {
+            displayNumber = displayNumber.slice(0, -1);
+        }
+        display.textContent = displayNumber;
+    } else if (Object.keys(operators).includes(pressedKey)) {
+        operatorUpdate({ target: { textContent: operators[pressedKey] } });
+    } else if (pressedKey === "Enter" || pressedKey === "NumpadEnter") {
+        performEquals();
+    }
+}
