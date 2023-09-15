83dea593a7bf0c5bc84170f6fbb9162fbdd03afe
Saad Qayyum <s.qayyum2003@gmail.com>
2023-09-15 19:21:48 +0100

    added the number buttons display updating

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,4 +1,4 @@
-fetchTheme("defaulr").then(applyTheme);
+fetchTheme("salad").then(applyTheme);
 
 const display = document.querySelector(".display > .digit");
 display.textContent = 0;
@@ -10,5 +10,23 @@ operatorDisplay.textContent = "";
 const ac = document.querySelector(".ac");
 ac.addEventListener("click", (e) => {
     display.textContent = 0;
-    operatorDisplay = "";
+    operatorDisplay.textContent = "";
 });
+
+const numbers = document.querySelectorAll(".number");
+numbers.forEach((number) => {
+    number.addEventListener("click", updateDisplayScreen);
+});
+
+
+function updateDisplayScreen(e){
+    const number = e.target;
+    let displayNumber = display.textContent;
+    displayNumber += number.textContent;
+    displayNumber = +displayNumber;
+
+    if (displayNumber.toString().length <= 12) {
+        display.textContent = displayNumber;
+    }
+}
+
