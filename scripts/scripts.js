d5dac103ab0da6b633d3b86229cb2e7ee4943d23
Saad Qayyum <s.qayyum2003@gmail.com>
2023-09-15 15:50:16 +0100

    ac button display reset added

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,1 +1,14 @@
 fetchTheme("defaulr").then(applyTheme);
+
+const display = document.querySelector(".display > .digit");
+display.textContent = 0;
+
+const operatorDisplay = document.querySelector(".display > .operator");
+operatorDisplay.textContent = "";
+
+
+const ac = document.querySelector(".ac");
+ac.addEventListener("click", (e) => {
+    display.textContent = 0;
+    operatorDisplay = "";
+});
